c4234ab9ba597098db17ce642bd9834bb0166c02
Samuel-code-design <samuel11hoi@gmail.com>
2023-06-18 17:29:46 +0200

    improved chatbot

--- README.md
+++ README.md
@@ -1,15 +1,31 @@
-generate 50 helpfull questions about the netherlands that tourists could ask with answers
--do not make the questions too similar or specific
+# NLP chatbot
 
-examles are:
-- questions about currency
-- locations and distances,
-- customs and traditions
- legal matters like age restrictions or pentalties
-- realy famous or important people from the netherlands
-- food
-- also add some generall questions about the netherlands
-- make sure the answers are correct!, 
+- Name: Samuel Brouwer
+- Student number: 649186
+- python version: 3.10.4
 
-put them in a code block in json format like tis:
-[{"question": "...", "answer":"..."}]
+## description of the programme:
+
+This program is a chatbot that uses a knowledge graph generated from a Wikipedia page to provide responses to user queries. The chatbot leverages several libraries such as NetworkX, pandas, spaCy, textacy, pickle, wikipediaapi, matplotlib, keytotext, and Spellchecker.
+
+Here's a breakdown of the program:
+
+Importing Libraries: The program starts by importing the necessary libraries, including NetworkX, pandas, spaCy, textacy, pickle, wikipediaapi, matplotlib, keytotext, and Spellchecker.
+
+Setting up Language Models: Two language models are loaded: "k2t-base" from the keytotext library for generating answers, and "en_core_web_md" from spaCy for natural language processing tasks.
+
+Variables and File Paths: The program defines variables for the Wikipedia page name, graph file path, and Wikipedia file path based on the page name.
+
+Function: create_graph_from_wikipedia_page(): This function creates a knowledge graph from the Wikipedia page. It checks if the Wikipedia file already exists and reads the text from it. If not, it fetches the page text using the wikipediaapi library and saves it to the file. The function then extracts entities, relations, and objects from the text and creates a dataframe. It identifies the most common entity and filters the dataframe accordingly. It then creates a directed graph using NetworkX, saves it to a pickle file, and visualizes the graph using matplotlib.
+
+Function: load_graph(file_path): This function loads a graph from a pickle file.
+
+Function: preprocess_text(input_text): This function preprocesses the user's input text. It converts the text to lowercase, removes question marks, removes stopwords using spaCy's STOP_WORDS, performs spelling correction using the Spellchecker library, and returns the preprocessed text.
+
+Function: generate_answer(graph, query): This function generates an answer to the user's query using the knowledge graph. It calculates the similarity between the query and each node in the graph using spaCy's similarity metric. It finds the node with the highest similarity score and retrieves the answer based on the outgoing edges of that node. It uses the keytotext pipeline to generate a response by providing the source, relation, and destination as input. The function returns the generated response.
+
+Function: get_response(input_text): This function handles the user's input and generates the chatbot's response. If the input is 'exit', the function exits. It checks if the graph file exists and loads the graph if it does. If the graph file doesn't exist, it calls the create_graph_from_wikipedia_page() function to create the graph. It then preprocesses the user's input and calls the generate_answer() function to generate the chatbot's response. If no response is generated, a default message is returned. The function returns the chatbot's response.
+
+Chatbot Loop: The program enters a loop that continuously prompts the user for input. It calls the get_response() function with the user's input and prints the chatbot's response. The loop continues until the user inputs 'exit'. Finally, a closing message is displayed.
+
+Overall, the program creates a chatbot that uses a knowledge graph derived from a Wikipedia page to generate responses to user queries. It uses = natural language processing techniekes to provide answers to user questions.
--- chatbot.py
+++ chatbot.py
@@ -1,17 +1,89 @@
-import random
-import pandas as pd
 import networkx as nx
+import pandas as pd
 import spacy
+import textacy
+import pickle
+import wikipediaapi
 from spacy.lang.en.stop_words import STOP_WORDS
 from spellchecker import SpellChecker
-import json
+import os
+import matplotlib.pyplot as plt
+from keytotext import pipeline
 
-# Load English tokenizer, tagger, parser, and NER
+nlp_ai = pipeline("k2t-base")
 nlp = spacy.load("en_core_web_md")
 
-# Define possible user inputs and corresponding chatbot responses
-data = pd.read_json("netherlands_data.json")
-# print(data.head())
+wiki_page_name = "Presidency_of_Donald_Trump"
+graph_file_path = wiki_page_name + "_knowledge_graph.gpickle"
+wiki_file_path = wiki_page_name + "_page"
+
+def create_graph_from_wikipedia_page():
+    # Check if the wiki file exists
+    if os.path.exists(wiki_file_path):
+        with open(wiki_file_path, 'r') as file:
+            text_py = file.read()
+    else:
+        wiki_wiki = wikipediaapi.Wikipedia('en')
+        page_py = wiki_wiki.page(wiki_page_name)
+        text_py = page_py.text
+
+        with open(wiki_file_path, 'w', encoding='utf-8') as file:
+            file.write(text_py)
+
+    lst_docs = [sent for sent in nlp(text_py).sents]
+
+    ## extract entities and relations
+    dic = {"id": [], "text":[], "entity":[], "relation":[], "object":[]}
+
+    for i, sentence in enumerate(lst_docs):
+        for sent in textacy.extract.subject_verb_object_triples(sentence):
+            subj = " ".join(map(str, sent.subject))
+            obj  = " ".join(map(str, sent.object))
+            relation = " ".join(map(str, sent.verb))
+
+            dic["id"].append(i)
+            dic["text"].append(sentence.text)
+            dic["entity"].append(subj)
+            dic["object"].append(obj)
+            dic["relation"].append(relation)
+
+    ## create dataframe
+    dtf = pd.DataFrame(dic)
+
+    most_common_entity = dtf["entity"].value_counts().head().index[0]
+    f = most_common_entity
+    tmp = dtf[(dtf["entity"]==f) | (dtf["object"]==f)]
+
+    ## create small graph
+    G = nx.from_pandas_edgelist(tmp, source="entity", target="object", 
+                                edge_attr="relation", 
+                                create_using=nx.DiGraph())
+    
+    with open(graph_file_path, 'wb') as f:
+        pickle.dump(G, f)
+
+    ## plot
+    plt.figure(figsize=(15,10))
+    pos = nx.spring_layout(G, k=1)
+
+    node_color = ["red" if node==f else "skyblue" for node in G.nodes]
+    edge_color = ["red" if edge[0]==f else "black" for edge in G.edges]
+
+    nx.draw(G, pos=pos, with_labels=True, node_color=node_color, 
+            edge_color=edge_color,
+            node_size=2000, node_shape="o")
+
+    nx.draw_networkx_edge_labels(G, pos=pos, label_pos=0.5, 
+                            edge_labels=nx.get_edge_attributes(G,'relation'),
+                            font_size=12, font_color='black', alpha=0.6)
+    plt.show()
+
+    return G
+
+def load_graph(file_path):
+    with open(file_path, 'rb') as f:
+        G = pickle.load(f)
+    return G
 
 def preprocess_text(input_text):
     # Convert input text to lowercase
@@ -31,76 +103,63 @@ def preprocess_text(input_text):
     if query is None:
         query = preprocessed_text
 
+    # lemmenitizer
+
     return query
 
-def knowledge_graph(data):
-    graph = nx.Graph()
-    for question in data["question"]:
-        answer = data[data["question"] == question]["answer"].values[0]
-        question = preprocess_text(question)  # Preprocess the question
+def generate_answer(graph, query):
+    highest_score = 0
+    nodes = {}
+    for node in graph.nodes:
+        similarity = nlp(query).similarity(nlp(node))
+        if similarity > highest_score:
+            highest_score = similarity
+            nodes[node] = [highest_score, graph[node]]
+    
+    best_answer = max(nodes, key=nodes.get)
 
-        graph.add_node(question)
-        graph.add_node(answer)
-        graph.add_edge(question, answer, relation="True")
+    for source, destination, relation in graph.out_edges(best_answer, data=True):
+        if source == best_answer:
+            relationship = relation.get('relation')
 
-    return graph
+    response = None
+    train = {"do_sample": True, "num_beams": 4, "no_repeat_ngram_size": 3, "early_stopping": True, "max_new_tokens":20}
 
+    for source, destination, relation in graph.out_edges(best_answer, data=True):
+        if source == best_answer:
+            response = nlp_ai([source, relation.get('relation'), destination], **train)
+            break
+        elif(relation.get('relation') == relationship):
+            response = nlp_ai([source, relation.get('relation'), destination], **train)
+            break
+    if not response:
+        response = "Sorry, I have no information on that"
+    return response
 
-
-def add_synonyms(graph, synonyms):
-    for original, synonym_list in synonyms.items():
-        for synonym in synonym_list:
-            if graph.nodes.__contains__(synonym):
-                graph.add_edge(original, synonym, relation="Synonym")
-    return graph
-
-# Create the knowledge graph
-graph = knowledge_graph(data)
-
-print("Graph Nodes:", graph.nodes)
-print("Graph Edges:", graph.edges)
-
-# get a dictionary of synonyms
-with open('synonyms.json') as json_file:
-    # Load the JSON data
-    synonyms = json.load(json_file)
-
-# Add synonyms to the graph
-graph = add_synonyms(graph, synonyms)
-
-print("Graph Nodes:", graph.nodes)
-print("Graph Edges:", graph.edges)
-
-
-# Define function to get a response from the chatbot
 def get_response(input_text):
     if input_text == 'exit':
         return None
-
+    
+    # Check if the graph file exists
+    if os.path.exists(graph_file_path):
+        # Load the graph from the saved file
+        G = load_graph(graph_file_path)
+    else:
+        # Create the graph and save it
+        G = create_graph_from_wikipedia_page()
+    
     # preprocess text
     query = preprocess_text(input_text)  # Preprocess the user's input
 
-    highest_score = 0.0
-    most_similar = None
-    most_similar_question = None
+    # Add code here to generate the chatbot response using the loaded_graph
+    # return generate_answer(G, query)
+    answer = generate_answer(G, query)
+    if not answer:
+        return "I'm sorry i dont know the answer to that"
 
-    for graph_question in graph.nodes:
-        similarity = nlp(query).similarity(nlp(graph_question))
-        if similarity > highest_score:
-            highest_score = similarity
-            most_similar = graph[graph_question]  # Replace `graph.neighbors(graph_question)` with `graph[graph_question]`
-            most_similar_question = graph_question
-
-    if highest_score <= 0.75:
-        return "I'm sorry, we don't have an answer for this."
-    else:
-        print(f"The answer is similar to the question: {most_similar_question} with a score of {highest_score}")
-        return next(iter(most_similar))
-
+    return answer
 
 # Loop to continuously prompt the user for input and return chatbot responses
-print("ask me a question:\nexample:")
-print(data.head())
 while True:
     user_input = input("Question: ")
     chatbot_response = get_response(user_input)
